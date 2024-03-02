5ee53883c00ae706c46cbd54f7efc45741f117a6
Tashin2098 <md.tashin.rahman@g.bracu.ac.bd>
2024-03-02 09:44:06 +0600

    third commit

--- EasyEdu1/urls.py
+++ EasyEdu1/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from edu import views
 from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -37,4 +38,9 @@ urlpatterns = [
     path('admin-dashboard', views.admin_dashboard_view,name='admin-dashboard'),
     path('student-dashboard', views.student_dashboard_view,name='student-dashboard'),
     path('teacher-dashboard', views.teacher_dashboard_view,name='teacher-dashboard'),
+    path('password_reset',auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'),name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/uidb64/token/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+
 ]
